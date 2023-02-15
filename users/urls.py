7bfcd7eeedcc8cef9e3167e34d8a8e907e94f75d
Idel Fazletdinov <fvi-it@mail.ru>
2023-02-15 22:13:20 +0500

    Перевел функции  регистрации и создание профиля в представления на основе классов

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('profile/', views.profile, name='profile'),
+    path('signup/', views.SignUpCreateView.as_view(), name='signup'),
+    path('profile/', views.UserProfileView.as_view(), name='profile'),
     path('logout/', views.logout, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,9 +4,17 @@ from django.contrib.auth import (authenticate,
                                  logout as logout_base)
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.views.generic import CreateView, UpdateView
+from django.urls import reverse_lazy
+from typing import Any, Dict, Optional
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import UserLoginForm, SignUpFrom, UserProfileForm
 from products.models import Basket
+
+
+User = get_user_model()
 
 
 def login(request):
@@ -24,31 +32,62 @@ def login(request):
     return render(request, 'users/login.html', context=context)
 
 
-def signup(request):
-    form = SignUpFrom(request.POST or None)
-    if form.is_valid():
-        form.save()
-        messages.success(request, "Поздравляем, Вы успешно зарегистрировались!")
-        return redirect('users:login')
+class SignUpCreateView(CreateView):
+    model = User
+    template_name = 'users/signup.html'
+    form_class = SignUpFrom
+    success_url = reverse_lazy('users:login')
 
-    context = {'form': form}
-    return render(request, 'users/signup.html', context=context)
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Store - Регистрация'
+        return context
 
 
-@login_required
-def profile(request):
-    form = UserProfileForm(request.POST or None,
-                           request.FILES or None,
-                           instance=request.user)
-    if form.is_valid():
-        form.save()
-        return redirect('users:profile')
-    baskets = Basket.objects.filter(user=request.user)
+class UserProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'users/profile.html'
+    form_class = UserProfileForm
 
-    context = {'form': form, 'baskets': baskets}
-    return render(request, 'users/profile.html', context=context)
+    def get_success_url(self) -> str:
+        return reverse_lazy('users:profile', args=(self.object.id))
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Store - Личный кабинет'
+        context['baskets'] = Basket.objects.filter(user=self.object)
+        return context
+
+    def get_object(self, queryset=None):
+        object = User.objects.get(id=self.request.user.id)
+        return object
 
 
 def logout(request):
     logout_base(request)
     return redirect('home')
+
+
+# def signup(request):
+#     form = SignUpFrom(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         messages.success(
+#             request, "Поздравляем, Вы успешно зарегистрировались!")
+#         return redirect('users:login')
+
+#     context = {'form': form}
+#     return render(request, 'users/signup.html', context=context)
+
+# @login_required
+# def profile(request):
+#     form = UserProfileForm(request.POST or None,
+#                            request.FILES or None,
+#                            instance=request.user)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('users:profile')
+#     baskets = Basket.objects.filter(user=request.user)
+
+#     context = {'form': form, 'baskets': baskets}
+#     return render(request, 'users/profile.html', context=context)
